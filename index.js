407020ec9b9298cc87691a9508ceb5dacb778ff8
Gabucado <awsalinas@uc.cl>
2025-01-01 19:59:13 -0300

    Root & server start

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const parser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 const router = require('./router.js');
 
+
 const App = new Koa();
 const port = 3000;
 
+// Middlewares
 App.use(parser());
 App.use(cors());
+App.use(router.allowedMethods());
 App.use(router.routes());
+
+App.use(async (ctx) => {
+  ctx.body = {
+    message: 'Available resources',
+    resources: [
+      { ventas: '/ventas' },
+      { inventarios: '/inventarios' },
+      { productos: '/productos' },
+    ]
+  };
+});
+
+
 App.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
