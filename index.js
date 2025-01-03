71e734752b87b95f5574e4ec46afd88e138ab2fe
Gabucado <awsalinas@uc.cl>
2025-01-02 21:26:20 -0300

    🐋 Docker config: Environment vars  - port

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const Koa = require('koa');
 const parser = require('koa-bodyparser');
 const cors = require('@koa/cors');
 const router = require('./router.js');
+require('dotenv').config();
 
 
 const App = new Koa();
-const port = 3000;
+const port = process.env.SERVER_PORT || 3000;
 
 // Middlewares
 App.use(parser());
